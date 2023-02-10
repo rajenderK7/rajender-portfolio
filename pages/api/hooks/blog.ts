2ca7d54d5924aa5f0a0153f7b8d2552a2bc33b5a
Rajender <rajenderkatkuri123@gmail.com>
2023-02-11 01:24:16 +0530

    Ref: For isr validate req instead of sign

--- pages/api/hooks/blog.ts
+++ pages/api/hooks/blog.ts
@@ -1,19 +1,18 @@
-import { SIGNATURE_HEADER_NAME, isValidSignature } from "@sanity/webhook";
+import { isValidRequest } from "@sanity/webhook";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString() ?? "";
     const secret = process.env.SANITY_WEBHOOK_SECRET ?? "";
     // validate the signature
-    if (!isValidSignature(JSON.stringify(req.body), signature, secret)) {
+    if (!isValidRequest(req, secret)) {
       res.status(401).json({ success: false, message: "Invalid request" });
       return;
     }
 
     const { slug } = req.body;
-    await res.revalidate(`/blog/${slug}`);
     await res.revalidate(`/blog/`);
+    await res.revalidate(`/blog/${slug}`);
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(500).json({ error: "Something went wrong!" });
