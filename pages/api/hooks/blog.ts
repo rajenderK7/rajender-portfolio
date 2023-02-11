a6def680f6d1b2ba94b209b37489dbc49427e3a6
Rajender <rajenderkatkuri123@gmail.com>
2023-02-11 13:40:41 +0530

    Add ISR

--- pages/api/hooks/blog.ts
+++ pages/api/hooks/blog.ts
@@ -1,7 +1,11 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+type Data = {
+  message: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method !== "POST") {
     console.log("Must a POST request");
     return res.status(401).json({ message: "Must be a POST request" });
@@ -9,14 +13,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const secret = process.env.SANITY_WEBHOOK_SECRET ?? "";
   const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString() ?? "";
-  if (!isValidSignature(JSON.stringify(req.body), signature, secret)) {
-    return res.status(401).json({ message: "Invalid signature" });
-  }
+  // if (!isValidSignature(JSON.stringify(req.body), signature, secret)) {
+  //   return res.status(401).json({ message: "Invalid signature" });
+  // }
+
+  const validReq = req.query.secret === secret;
+  console.log(`valid req ${validReq}`);
+  console.log(`query secret ${req.query.secret}`);
+  // console.log(`Parse req ${JSON.parse(JSON.stringify(req))}`);
+  console.log(
+    `valid signature ${isValidSignature(
+      JSON.stringify(req.body),
+      signature,
+      secret
+    )}`
+  );
+  console.log(`my secret ${secret}`);
+  console.log(`signature ${signature}`);
+  console.log(`req.body ${req.body}`);
 
   try {
     const { slug } = req.body;
-    await res.revalidate(`/blog/${slug}`);
-    await res.revalidate("/blog/");
+    // await res.revalidate(`/blog/${slug}`);
+    // await res.revalidate("/blog/");
     console.log(`Revalidated slug: ${slug}`);
     res.status(200).json({ message: "Revalidation successful" });
   } catch (e) {
