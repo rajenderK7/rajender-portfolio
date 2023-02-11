bec5b6e5d286102706ab8e77ea63e09f60bd1a4d
Rajender <rajenderkatkuri123@gmail.com>
2023-02-11 11:09:13 +0530

    Ref: Update isr

--- pages/api/hooks/blog.ts
+++ pages/api/hooks/blog.ts
@@ -1,18 +1,21 @@
-import { isValidRequest } from "@sanity/webhook";
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const secret = process.env.SANITY_WEBHOOK_SECRET ?? "";
+    const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString() ?? "";
     // validate the signature
-    if (!isValidRequest(req, secret)) {
-      res.status(401).json({ success: false, message: "Invalid request" });
-      return;
+    if (!isValidSignature(JSON.stringify(req.body), signature, secret)) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid request" });
     }
 
     const { slug } = req.body;
-    await res.revalidate(`/blog/`);
     await res.revalidate(`/blog/${slug}`);
+    await res.revalidate(`/blog/`);
+    console.log(`Revalidated slug: ${slug}`);
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(500).json({ error: "Something went wrong!" });
